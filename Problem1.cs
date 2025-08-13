d5405651bc3c6242fa971e9e728c34ba750b76d8
Gian-MP <202480071@psu.palawan.edu.ph>
2025-08-13 21:53:22 +0800

    Update Problem1.cs

--- Problem1.cs
+++ Problem1.cs
@@ -1,1 +1,13 @@
+using System;
 
+public class Problem1
+{
+    public static void Main(string[] args)
+    {
+        int number = Console.Read();
+        if (number % 2 == 0){
+            Console.WriteLine("The number you input is even");
+        }
+        
+    }
+}
